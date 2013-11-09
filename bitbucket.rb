e04ee718bd7158ca31b7209ba61958ede9217755
msl <robin@mrrobinsmith.com>
2013-11-09 13:46:03 +0000

    Auto-update

--- bitbucket.rb
+++ bitbucket.rb
@@ -1,22 +1,20 @@
 require 'oauth'
-
 require 'yaml'
 
-require_relative 'settings'
-
-
-@callback_url = "https://bitbucket.org/api/1.0/oauth/callback"
 
 config = YAML.load_file("config.yml")
 consumer_key = config['consumer_key']
 consumer_secret = config['consumer_secret']
+site = config['site']
+callback = config['callback']
 
-puts consumer_key
 
-#@consumer = OAuth::Consumer.new(ENV[CONSUMER_KEY], ENV[CONSUMER_KEY], :site => "https://bitbucket.org")
-#
-#@request_token = @consumer.get_request_token(:oauth_callback => @callback_url)
-#
+@consumer = OAuth::Consumer.new(consumer_key, consumer_secret, :site => site)
+
+
+@request_token = @consumer.get_request_token(:oauth_callback => callback)
+
 #puts @request_token
+
 ##session[:request_token] = @request_token
 ##redirect_to @request_token.authorize_url(:oauth_callback => @callback_url)
