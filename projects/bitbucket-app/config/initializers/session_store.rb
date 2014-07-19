510f5c166d265b996858427fe4548c374f0ec455
msl <msl@mercury.local>
2014-07-19 14:23:50 -0700

    Auto-update

--- projects/bitbucket-app/config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-BitbucketApp::Application.config.session_store :cookie_store, key: '_bitbucket-app_session'
--- projects/password/password.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'securerandom'
-
-puts SecureRandom.urlsafe_base64(14)
